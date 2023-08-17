1a662be28bb079b99f7923210045bcc3f449ede6
ConnorSimons <142337753+ConnorSimons@users.noreply.github.com>
2023-08-17 09:05:24 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Powershell Guide`,
+    description: `Join me in exploring the world of powershell.`,
+    author: `@Connor`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
