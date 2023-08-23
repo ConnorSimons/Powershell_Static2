026ccadadb513a0e5831b0a94f85d89599dee0dd
ConnorSimons <142337753+ConnorSimons@users.noreply.github.com>
2023-08-22 17:22:17 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,71 +1,59 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
 const IndexPage = () => (
   <Layout>
-    <header>
-      <h1>My PowerShell Learning Journey</h1>
-    </header>
-    <section id="about-me">
-      <h2>About Me</h2>
-      <p>
-        Hello, I am Connor Simons. When I'm not diving into the world of PowerShell, you'll often find me immersed in the world of games, both on-screen and with my trusty RC cars. I also have a deep appreciation for the great outdoors, and you can often catch me exploring the breathtaking landscapes of the Pacific Northwest through hiking.
-      </p>
+    <section id="home">
+      <header>
+        <h1>My PowerShell Learning Journey</h1>
+      </header>
     </section>
-    <section id="overview">
-      <h2>Overview</h2>
+    <section id="learning-method">
+      <h2>My Learning Method</h2>
       <p>
-        Welcome to my PowerShell learning journey! In this blog, I'll share my experiences and insights as I delve into the world of PowerShell scripting.
+        In my journey of mastering PowerShell, I've adopted a systematic learning approach. I believe in continuous learning, and I've been using a combination of online tutorials, video courses, and interactive platforms to enhance my PowerShell skills.
       </p>
       <p>
-        We'll cover key topics including:
+        Some of the resources I've found particularly helpful include:
       </p>
       <ul>
         <li>
-          <strong>PowerShell Help System:</strong> Mastering the built-in help system to quickly access documentation and learn about cmdlets and their usage.
+          <strong>Online Tutorials:</strong> Websites like PowerShell.org and Microsoft Learn offer comprehensive tutorials and guides for beginners and advanced users.
         </li>
         <li>
-          <strong>Functions:</strong> Creating and utilizing functions to modularize your code, enhance reusability, and streamline your PowerShell scripts.
+          <strong>Video Courses:</strong> Platforms like Pluralsight and Udemy provide in-depth video courses on PowerShell scripting and automation.
         </li>
         <li>
-          <strong>PowerShell Remoting:</strong> Harnessing the power of remoting to manage and control remote machines, execute commands, and retrieve information.
+          <strong>Interactive Labs:</strong> I've taken advantage of platforms like Try PowerShell to practice my skills in a real-world environment.
         </li>
       </ul>
       <p>
-        Whether you're a beginner or an experienced IT professional, this blog will guide you through essential PowerShell concepts and techniques. Let's dive in and discover the capabilities of PowerShell together!
+        This diverse approach has allowed me to grasp PowerShell concepts effectively and apply them to real-world scenarios.
       </p>
     </section>
-    <section id="help-system">
-      <h2>Exploring the Help System</h2>
+    <section id="challenge-labs">
+      <h2>Challenge Labs</h2>
       <p>
-        One of the essential aspects of PowerShell is its extensive help system,
-        which provides detailed information about commands and concepts.
-        Let's dive into some basics of using the help system.
+        As part of my learning journey, I've completed various challenge labs from our PowerShell course. These labs have been instrumental in applying theoretical knowledge to practical tasks. Let's explore some of these challenge labs and see how I approached and solved the challenges.
       </p>
-      {/* Rest of the Help System content */}
+      {/* Add details about each challenge lab */}
     </section>
-    <section id="functions">
-      <h2>Mastering Functions</h2>
+    <section id="my-projects">
+      <h2>My Projects</h2>
       <p>
-        Functions are like mini-programs within PowerShell that allow you to create reusable blocks of code.
-        They're like building blocks that you can use over and over again in your scripts.
-        Let's explore how functions work by looking at their structure and how you can use them.
+        In addition to coursework, I've embarked on some exciting PowerShell projects to further enhance my skills. These projects showcase my creativity and ability to develop automation solutions using PowerShell.
       </p>
-      {/* Rest of the Functions content */}
+      {/* Add details about each project */}
     </section>
-    <section id="powershell-remoting">
-      <h2>PowerShell Remoting</h2>
+    <section id="contact">
+      <h2>Contact Me</h2>
       <p>
-        PowerShell Remoting is a powerful feature that allows you to execute commands on remote computers.
-        It's like having the ability to control other machines from the comfort of your own computer.
-        Let's explore how PowerShell Remoting works and how you can use it.
+        If you're interested in connecting, collaborating, or learning more about my PowerShell journey, feel free to get in touch!
       </p>
-      {/* Rest of the PowerShell Remoting content */}
+      {/* Add contact information */}
     </section>
   </Layout>
 )
@@ -75,6 +63,6 @@ const IndexPage = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="Contents" />
 
 export default IndexPage
