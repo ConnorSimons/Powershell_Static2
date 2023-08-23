0143784bf1b0b505b55de58f65f15edeabac4445
ConnorSimons <142337753+ConnorSimons@users.noreply.github.com>
2023-08-22 17:45:53 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,11 +42,61 @@ const IndexPage = () => (
     </section>
     <section id="challenge-labs">
       <h2>Challenge Labs</h2>
-      <p>
-        As part of my learning journey, I've completed various challenge labs from our PowerShell course. These labs have been instrumental in applying theoretical knowledge to practical tasks. Let's explore some of these challenge labs and see how I approached and solved the challenges.
-      </p>
-      {/* Add details about each challenge lab */}
-    </section>
+    <h2>Project: Managing Windows Services and Print Spooler</h2>
+  <p>
+    In this section, I'll walk you through my journey of creating a PowerShell script to manage Windows services and address print spooler challenges. This project provided an opportunity to deepen my PowerShell skills and learn more about service management and file manipulation.
+  </p>
+  <h3>Learning and Progression</h3>
+  <p>
+    I started by familiarizing myself with the <code>Get-Service</code> cmdlet and its capabilities to retrieve information about Windows services. I learned about filtering services based on their status and selected only running services using the <code>Where-Object</code> cmdlet.
+  </p>
+  <p>
+    My progression continued with the creation of an HTML file to display the list of running services. I utilized the <code>ConvertTo-Html</code> cmdlet to format the service details and wrote the HTML content to a file named based on the current date.
+  </p>
+  <p>
+    Next, I explored JSON formatting using the <code>ConvertTo-Json</code> cmdlet. I extracted essential details from the running services and stored them in a JSON file for further analysis and usage.
+  </p>
+  <p>
+    To maintain a clean storage space, I developed code to remove files older than 30 days from the designated folders using the <code>Get-ChildItem</code> and <code>Remove-Item</code> cmdlets.
+  </p>
+  <p>
+    With a foundational understanding of managing services, I created a script that checked for files created today in the services and details folders. If the required files were not found, an error message would be displayed.
+  </p>
+  <p>
+    Finally, I tackled the challenge of automating the restart of the print spooler service when old print jobs were detected in the spooler folder. I incorporated parameters to adjust the maximum age of files to be considered old.
+  </p>
+  <h3>Code Snippets</h3>
+  <div class="code-section">
+    <div class="code-image">
+      <StaticImage
+        src="../images/code-export-services.png"
+        alt="Exporting Services to Files"
+        placeholder="blurred"
+      />
+    </div>
+    <div class="code-text">
+      <p>Script to Export Services to Files:</p>
+      <pre><code>{/* Place your PowerShell script here */}</code></pre>
+    </div>
+  </div>
+
+  <div class="code-section">
+    <div class="code-image">
+      <StaticImage
+        src="../images/code-restart-spooler.png"
+        alt="Checking Old Spool Files and Restarting Print Spooler"
+        placeholder="blurred"
+      />
+    </div>
+    <div class="code-text">
+      <p>Script to Check Old Spool Files and Restart Print Spooler:</p>
+      <pre><code>{/* Place your PowerShell script here */}</code></pre>
+    </div>
+  </div>
+  <p>
+    Through this project, I deepened my understanding of Windows services, file manipulation, and parameterized scripts. The journey helped me enhance my PowerShell skills and build practical solutions to real-world challenges.
+  </p>
+</section>
     <section id="my-projects">
       <h2>My Projects</h2>
       <p>
